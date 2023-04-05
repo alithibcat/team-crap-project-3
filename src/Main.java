ef955186b25870abd85bc2beb89e976c6da4d721
colli <collinkovac01@gmail.com>
2023-04-05 16:58:10 -0500

    creation of RQ and populating it with task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //thread creation and population of RQ by Collin
+        int t = (int) (Math.random() * (25 - 1) + 1);
+        Queue<Task> readyQueue = new LinkedList<>();
+        for(int i = 0; i < t; i++){
+            Task thread = new Task(i);
+            readyQueue.add(thread);
+        }
     }
 }
